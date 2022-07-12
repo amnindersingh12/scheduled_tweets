bd5b047fbba47b733acd7a043847678e73d40b41
amnindersing12 <amnindersingh58611@gmai.com>
2022-07-12 15:18:25 +0530

    Added Sign In and Sign Up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 
 
 Rails.application.routes.draw do
-
+  
+  #  About page
   get "about", to: "about#index" 
+  
+  # Sign up page
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  # Sign in page
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  
+  # Home page
   root to: "main#index"
+
+  # Logout 
+  delete "logout", to: "sessions#destroy"
 
 
 end
