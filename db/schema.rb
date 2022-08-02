80469df3396649fffd5250005009ffbfb13005ea
amnindersingh12 <amnindersingh58611@gmail.com>
2022-08-02 13:18:07 +0530

    Updating tweet_id column to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_28_080442) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_074605) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -70,7 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_28_080442) do
     t.integer "user_id", null: false
     t.text "body"
     t.datetime "publish_at"
-    t.string "tweet_id"
+    t.integer "tweet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tweets_on_user_id"
