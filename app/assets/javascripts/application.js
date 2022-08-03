8a5c26c1655d9c3754369a0745a9790c4ec98cc0
amnindersingh12 <amnindersingh58611@gmail.com>
2022-08-03 20:27:44 +0530

    fixed twice tweet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,4 @@
 // This is a manifest file that will be compiled into application.js, which will include all the files
-//= require jquery
-//= require jquery_ujs
 //= require jquery3
 //= require rails-ujs
 //= require activestorage
